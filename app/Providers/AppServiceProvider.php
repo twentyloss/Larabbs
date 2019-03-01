e2560c5880d2e2ee826e6047afd5630e7c200e10
0221 <1252153341@qq.com>
2019-03-01 16:05:58 +0800

    站点权限部署

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (app()->isLocal()) {
+            $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+
+        }
     }
 
     /**
