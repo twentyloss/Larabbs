c5e506c931010ea87a329e3158175447a3eec3c7
0221 <1252153341@qq.com>
2019-03-01 15:32:45 +0800

    删除评论

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -32,4 +32,10 @@ class TopicObserver
             dispatch(new TranslateSlug($topic));
         }
     }
+
+    public function deleted(Topic $topic)
+    {
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
+    }
+
 }
