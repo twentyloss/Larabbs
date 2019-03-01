ff7d854db25a1c3f8e7feb63d1ec0790528f900a
0221 <1252153341@qq.com>
2019-03-01 15:09:44 +0800

    支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
 use App\Models\Reply;
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
+
 {
     use Queueable;
     public $reply;
@@ -20,7 +21,7 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database','mail'];
     }
     public function toDatabase($notifiable)
     {
@@ -37,5 +38,13 @@ class TopicReplied extends Notification
             'topic_id' => $topic->id,
             'topic_title' => $topic->title,
         ];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+        return (new MailMessage)
+            ->line('你的话题有新回复！')
+            ->action('查看回复', $url);
     }
 }
